5ec47cf75ce2e1acd75ac85b2bd3d0e98e8fca3c
Aman <92225430+amantt10@users.noreply.github.com>
2025-09-03 11:28:57 -0500

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    domains: [
+      "api.microlink.io",
+    ],
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
